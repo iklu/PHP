6240e387313a0dd33a286c8b628c9cbee1868968
Ovidiu Dragoi <ovidiu.dragoi@xivic.com>
2016-10-20 12:39:01 +0300

    facebook

---
+++ FACEBOOK_SDK/autoload/Autoload.php
@@ -0,0 +1,31 @@
+<?php
+function autoload($class)
+{
+	$paths = explode(PATH_SEPARATOR, get_include_path());
+	$paths[]= "./src/";	
+	$flags = PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE;
+	$file = str_replace("\\", DIRECTORY_SEPARATOR, trim($class, "\\")).".php";
+	foreach($paths as $path)
+	{
+	      $combined = ".".$path.DIRECTORY_SEPARATOR.$file;
+          
+           echo $path.'<br>';
+	       
+		if(file_exists($combined))
+		{
+			include_once($combined);
+			return;
+		}
+	}
+	throw new Exception("{$class} the class was not found");
+}
+class Autoloader 
+{
+	public static function autoload($class)
+	{
+		autoload($class);
+	}
+}
+spl_autoload_register('autoload');
+spl_autoload_register(array('autoloader', 'autoload'));
+?>
